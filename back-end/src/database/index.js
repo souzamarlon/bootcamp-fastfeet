1a4e74ee9bc49b880b6ad5b7e637a78094a87d8d
Marlon <marlon.bobi@hotmail.com>
2020-02-08 21:56:59 -0300

    Created the migrations and User table

---
+++ back-end/src/database/index.js
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(databaseConfig);
+    this.init();
+    this.associate();
+  }
+
+  init() {
+    models.forEach(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
+  }
+}
+
+export default new Database();
