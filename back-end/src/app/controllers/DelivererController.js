a0fba0a5f0824962771fd064e95d7972e7b32db6
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-02-11 07:55:04 -0300

    Added index, update and delete in the DelivererController

--- back-end/src/app/controllers/DelivererController.js
+++ back-end/src/app/controllers/DelivererController.js
@@ -3,6 +3,12 @@ import * as Yup from 'yup';
 import Deliverer from '../models/Deliverer';
 
 class DelivererController {
+  async index(req, res) {
+    const delivererData = await Deliverer.findAll();
+
+    return res.json(delivererData);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -16,11 +22,29 @@ class DelivererController {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
+    // TODO - Check if the email is already using for someone.
+
     const delivererFields = await Deliverer.create(req.body);
 
-    console.log(delivererFields);
-
     return res.json(delivererFields);
+  }
+
+  async update(req, res) {
+    const delivererId = req.params.id;
+
+    const { name, email, avatar_id } = req.body;
+
+    const delivererData = await Deliverer.findByPk(delivererId);
+
+    return res.json(await delivererData.update({ name, email, avatar_id }));
+  }
+
+  async delete(req, res) {
+    const delivererId = req.params.id;
+
+    const delivererData = await Deliverer.findByPk(delivererId);
+
+    return res.json(await delivererData.destroy());
   }
 }
 
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -22,7 +22,10 @@ routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 
+routes.get('/deliverers', DelivererController.index);
 routes.post('/deliverers', DelivererController.store);
+routes.put('/deliverers/:id', DelivererController.update);
+routes.delete('/deliverers/:id', DelivererController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
