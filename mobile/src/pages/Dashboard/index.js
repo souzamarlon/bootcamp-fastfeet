43385a6f72031479c6bc5aee506e8459b8b267b2
Marlon <marlon.bobi@hotmail.com>
2020-03-09 19:48:35 -0300

    Doing the Dashboard!

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -2,21 +2,28 @@ import React from 'react';
 import { Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
-import { Container, Header, Avatar } from './styles';
+import { Container, Header, Avatar, Welcome, Name } from './styles';
 
 export default function Dashboard() {
   const { profile } = useSelector(state => state.auth);
 
-  console.tron.log(profile.avatar.url);
+  console.tron.log(profile.name);
   return (
     <Container>
       <Header>
-        <Avatar
-          source={{
-            uri: profile.avatar.url,
-          }}
-        />
-        <Text>test</Text>
+        <>
+          <Avatar
+            source={{
+              uri: profile.avatar.url
+                ? profile.avatar.url
+                : `https://avatars.dicebear.com/v2/initials/${profile.name}.svg`,
+            }}
+          />
+        </>
+        <>
+          <Welcome>Bem vindo de volta,</Welcome>
+        </>
+        <Name>{profile.name}</Name>
       </Header>
     </Container>
   );
--- mobile/src/pages/Dashboard/styles.js
+++ mobile/src/pages/Dashboard/styles.js
@@ -5,23 +5,44 @@ export const Container = styled.View`
   background: transparent;
 `;
 export const Header = styled.View`
-  /* display: flex; */
-  padding-top: 10px;
-  width: 150px;
-  height: 150px;
+  padding-top: 20px;
+  margin-left: 20px;
+  width: 300px;
+  display: flex;
+
+  /* align-items: center; */
+  /* justify-content: space-between; */
 
   /* background: #000000; */
+
+  flex-direction: row;
+  /* text-align: left; */
 `;
 
 export const Avatar = styled.Image`
   /* background: #000000; */
-
-  padding-top: 10px;
-  padding-left: 100px;
-
+  /* padding-top: 10px; */
+  /* padding-left: 100px; */
   width: 100px;
   height: 100px;
-  border-radius: 25px;
+  border-radius: 50px;
+`;
+export const Welcome = styled.Text`
+  color: #666666;
+  font-size: 12px;
+  height: 16px;
+  display: flex;
+`;
+
+export const Name = styled.Text`
+  /* padding-top: 30px; */
+  background: #000000;
+
+  color: #444444;
+  font-size: 22px;
+  height: 29px;
+  font-weight: bold;
+  display: flex;
 `;
 
 export const List = styled.FlatList.attrs({
