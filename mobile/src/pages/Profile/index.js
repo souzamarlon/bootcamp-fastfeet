88d519622179a3a59a8859f6e42aa66fc423b257
Marlon <marlon.bobi@hotmail.com>
2020-03-13 00:06:27 -0300

    Finished the Profile page!

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import { View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { useMemo } from 'react';
+import { parseISO, format } from 'date-fns';
+import { zonedTimeToUtc } from 'date-fns-tz';
 
-// import { Container } from './styles';
+import pt from 'date-fns/locale/pt';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { signOut } from '~/store/modules/auth/actions';
+
+import { Container, Avatar, Name, Text, LogoutButton } from './styles';
 
 export default function Profile() {
-  return <View />;
+  const dispatch = useDispatch();
+  const { profile } = useSelector(state => state.auth);
+
+  const dateParsed = useMemo(() => {
+    return format(parseISO(profile.created_at), 'dd/MM/yyyy', {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [profile.created_at]);
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+  return (
+    <Container>
+      <Avatar
+        source={{
+          uri: profile.avatar.url
+            ? profile.avatar.url
+            : `https://avatars.dicebear.com/v2/initials/${profile.name}.svg`,
+        }}
+      />
+      <Name>Nome completo</Name>
+      <Text>{profile.name}</Text>
+      <Name>Email</Name>
+      <Text>{profile.email}</Text>
+      <Name>Data de cadastro</Name>
+      <Text>{dateParsed}</Text>
+
+      <LogoutButton onPress={handleLogout}>Logout</LogoutButton>
+    </Container>
+  );
 }
 
 Profile.navigationOptions = {
--- mobile/src/pages/Profile/styles.js
+++ mobile/src/pages/Profile/styles.js
@@ -1,3 +1,44 @@
 import styled from 'styled-components/native';
+import Button from '~/components/Button';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  align-content: center;
+  margin-left: 36px;
+`;
+
+export const Avatar = styled.Image`
+  align-self: center;
+  margin-top: 50px;
+  width: 150px;
+  height: 150px;
+  border-radius: 70px;
+  /* flex: 1; */
+`;
+
+export const Name = styled.Text`
+  padding-top: 20px;
+  color: #666666;
+  font-size: 12px;
+  width: 158px;
+  /* height: 54px; */
+  display: flex;
+`;
+
+export const Text = styled.Text`
+  line-height: 23px;
+
+  color: #444444;
+  font-size: 22px;
+  font-weight: bold;
+  /* width: 158px; */
+  /* height: 54px; */
+  display: flex;
+`;
+
+export const LogoutButton = styled(Button)`
+  margin-top: 30px;
+  width: 293px;
+  height: 40px;
+  background: #e74040;
+  color: #ffffff;
+`;
