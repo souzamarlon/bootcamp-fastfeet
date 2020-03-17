3383afb028b32a4522ccef0071e837ce32c63ce4
Marlon <marlon.bobi@hotmail.com>
2020-03-17 02:28:58 -0300

    Setting react-native-camera in the project!

--- mobile/src/pages/Dashboard/PackageDetails/ConfirmDelivery/index.js
+++ mobile/src/pages/Dashboard/PackageDetails/ConfirmDelivery/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { RNCamera, FaceDetector } from 'react-native-camera';
+import { RNCamera } from 'react-native-camera';
 
 import { Container } from './styles';
 
@@ -11,10 +11,75 @@ import api from '~/services/api';
 
 export default function ConfirmDelivery({ navigation }) {
   // const data = navigation.getParam('data');
+  const [flash, setFlash] = useState('off');
+  const [zoom, setZoom] = useState(0);
+  const [autoFocus, setAutoFocus] = useState('on');
+  const [depth, setDepth] = useState(0);
+  const [type, setType] = useState('back');
+  const [permission, setPermission] = useState('undetermined');
+  const cameraRef = useRef(null);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    dummy: {
+      backgroundColor: '#76e',
+      height: 250,
+      width: '100%',
+      position: 'absolute',
+      top: 0,
+    },
+    preview: {
+      height: 500,
+      width: 300,
+      borderRadius: 4,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    capture: {
+      flex: 0,
+      backgroundColor: '#76e',
+      borderRadius: 5,
+      padding: 15,
+      paddingHorizontal: 100,
+      alignSelf: 'center',
+      margin: 50,
+    },
+  });
+
+  async function takePicture() {
+    if (cameraRef) {
+      const options = { quality: 0.5, base64: true };
+      const data = await cameraRef.current.takePictureAsync(options);
+      Alert.alert(data.uri);
+    }
+  }
 
   return (
     <PagesBackground>
-      <Container />
+      <Container>
+        <View style={styles.container}>
+          <View style={styles.dummy} />
+          <RNCamera
+            ref={cameraRef}
+            style={styles.preview}
+            type={type}
+            flashMode={flash}
+          />
+        </View>
+        <View
+          style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}
+        >
+          <TouchableOpacity onPress={takePicture} style={styles.capture}>
+            <Text style={{ fontSize: 14, color: '#fff' }}> Enviar </Text>
+          </TouchableOpacity>
+        </View>
+      </Container>
     </PagesBackground>
   );
 }
