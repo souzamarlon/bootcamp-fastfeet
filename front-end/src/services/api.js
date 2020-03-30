87374faa6c874505ac78a9cbf88c4098f7c4384a
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-03-30 15:59:56 -0300

    Added back end from heroku!

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'http://localhost:3333',
+    // baseURL: 'https://bootcamp-fastfeet-backend.herokuapp.com',
 });
 
 export default api;
